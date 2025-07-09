fd9b8a32317c66f4679fe7283bedca7f14065a9b
saloni <salonijaiswal.7412@gmail.com>
2025-07-09 16:07:29 +0530

    mongo uri

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,17 @@
-// MongoDB connection
-const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-dotenv.config();
-
-const connectDB=async()=>{
-    await mongoose.connect("mongodb://127.0.0.1:27017/readrave")
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log("DB connection error:", err));
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("DB connection error:", error);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;

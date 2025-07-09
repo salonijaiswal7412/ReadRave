70ca7663dc3bde59289cd6d4e640caf19591b5d0
saloni <salonijaiswal.7412@gmail.com>
2025-07-09 13:51:43 +0530

    final changes

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://read-rave.vercel.app/'],
   credentials:true}));
 
 app.use(express.urlencoded({ extended: true }));
